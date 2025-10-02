e04045930e6221ed460c101c215d49e519680a06
xcnnon <gavin.tung@ciit.edu.ph>
2025-10-02 13:14:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -48,6 +48,66 @@ app.get("/customers", async (req, res) => {
 });
 //Retail Store
 
+app.get("/customers", async (req, res) => {
+   });
+
+app.post("/customers", async (req, res) => {
+  try {
+    const { username, email, phone, password, first_name, last_name,address } = req.body;
+  
+    if( !username || !email || !password || !first_name || !last_name){
+      return res.status(400).json({message: "Missing required fields.",
+      fields: {username, email, password, first_name, last_name},
+      });
+    }
+
+    const newCustomer = { ...req.body, created_at: new Date() };
+    const result = await customerCollections.insertOne(newCustomer);
+    
+
+    res.status(201).json({
+      data: result,
+      message: "Customer created successfully.",
+    });
+  } catch(error) {
+    res 
+      .status(500)
+      .json({ message: "Internal Server Error.", error: error.message });
+  }
+});
+
+app.put("/customers/:id", async (req, res) => {
+  try {
+    const { username, email, phone, password, first_name, last_name,address } = req.body;
+  
+    if( !username || !email || !password || !first_name || !last_name){
+      return res.status(400).json({message: "Missing required fields.",
+      fields: {username, email, password, first_name, last_name},
+      });
+    }
+
+    const customerID = new mongodb.ObjectId(req.params.id);
+    const updatedCustomer = { ...req.body, updated_at: new Date() };
+    const result = await customerCollections.updateOne(
+      { _id: customerID },
+      { $set: updatedCustomer }
+    );
+    
+
+    res.status(201).json({
+      data: result,
+      message: "Customer created successfully.",
+    });
+  } catch(error) {
+    res 
+      .status(500)
+      .json({ message: "Internal Server Error.", error: error.message });
+  }
+
+});
+
+
+
 connectToDatabase().then(() => {
   app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
